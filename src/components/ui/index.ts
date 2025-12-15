a55b9b01c84bca756212dcd060d5ef58234112b4
Jairo Calcina <j.calcina.v@gmail.com>
2025-12-14 22:05:40 -0300

    feat(components): add ui components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,14 @@
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Spinner, FullPageSpinner } from "./Spinner";
+export { Card, CardHeader, CardTitle, CardContent, CardFooter } from "./Card";
+export { Modal } from "./Modal";
+export { ErrorMessage, EmptyState } from "./ErrorMessage";
+export {
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableCell,
+} from "./Table";
